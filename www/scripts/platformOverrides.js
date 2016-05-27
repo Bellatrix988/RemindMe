30886162b697b3bfaa2952151bed8988aeaf9e4a
Марина Юрова < >
2016-05-27 13:27:10 +0300

    Add all files

---
+++ www/scripts/platformOverrides.js
@@ -0,0 +1,9 @@
+/*
+<<<<<<< HEAD
+	Этот файл заменяется кодом платформы из папки /merges.
+	Дополнительные сведения см. по ссылке http://taco.visualstudio.com/ru-ru/docs/configure-app/#Content.
+=======
+	This file is replaced with platform-specific code from the /merges folder.
+	More info at http://taco.visualstudio.com/en-us/docs/configure-app/#Content.
+>>>>>>> 87fd9b878f1de319ede1ec65b6a5a95cb28da81e
+*/
